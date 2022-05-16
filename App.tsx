5202e20bcc4ddf3a07ca7cd36b0de60c30eeb3e3
Kabaji Egara <kabzegara@gmail.com>
2022-05-16 17:35:16 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      source={{ uri: 'http://mambozuri.com/' }}
+    />
   );
 }
 
